e1242e9dc5bf82c739b65e0bb54e218d4d172ffb
raselrzl <rasel6041@gmail.com>
2026-01-24 21:45:14 +0100

    added timelog statu changed colour

--- app/components/ExpertiseSection.tsx
+++ app/components/ExpertiseSection.tsx
@@ -43,7 +43,7 @@ const CAPABILITIES: CapabilityItem[] = [
 
 export const ExpertiseSection: React.FC = () => {
   return (
-    <section id="about" className="relative w-full bg-black text-white py-20">
+    <section id="about" className="relative w-full bg-black text-white py-6">
       
       <div className="max-w-7xl mx-auto px-6 md:px-12">
         
